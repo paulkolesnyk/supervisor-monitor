0b7b2ab471e8d7c4455f667ae255e4c6bfce71ec
Paul Kolesnyk <kolesnyk.paul99@gmail.com>
2018-11-20 13:50:37 +0200

    WIP: Fixed bugs. Added memory-checker

--- supervisor_monitor/monitor.py
+++ supervisor_monitor/monitor.py
@@ -2,15 +2,16 @@ import subprocess
 import click
 import threading
 import logging
+import requests
 from time import sleep
-from requests import get
 
 
 class Monitor:
-    def __init__(self, url, request_timeout, program_name):
+    def __init__(self, url, max_memory_size, request_timeout, program_name):
         self.url = url
         self.request_timeout = request_timeout
         self.program_name = program_name
+        self.max_memory_size = max_memory_size
         self.pscommand = 'ps -q %s -o "size" --no-header'
         self.logger = logging.getLogger('supervisor-monitor')
 
@@ -30,14 +31,23 @@ class Monitor:
     def run_monitor(self):
         while 1:
             try:
-                pid = subprocess.run(['supervisorctl', 'pid', self.program_name]).STDOUT
+                pid = subprocess.run(
+                    ['supervisorctl', 'pid', self.program_name],
+                    capture_output=True).stdout
                 if not pid:
-                    raise Exception('Error: pid not found!')
+                    raise Exception('Error getting pid of "{}" program'.format(self.program_name))
 
-                status = subprocess.run(self.pscommand % pid, shell=True).STDOUT
+                status = subprocess.run(
+                    self.pscommand % pid,
+                    shell=True,
+                    capture_output=True).stdout
                 if not status:
-                    raise Exception('Error: error getting status')
-                self.logger.info(status)
+                    raise Exception('Error getting status of "{}" program (pid - {})'.format(
+                        self.program_name,
+                        pid))
+                if int(status) / 1024 > int(self.max_memory_size):
+                    self.restart_program()
+
             except Exception as e:
                 self.logger.error(e)
 
@@ -46,7 +56,8 @@ class Monitor:
     def run_checker(self):
         while 1:
             try:
-                get(self.url)
+                r = requests.get(self.url)
+                assert r.status_code < 500
             except Exception:
                 self.restart_program()
 
@@ -54,28 +65,37 @@ class Monitor:
 
     def restart_program(self):
         try:
-            status = subprocess.run(['supervisorctl', 'restart', self.program_name])
+            status = subprocess.run(
+                ['supervisorctl', 'restart', self.program_name],
+                capture_output=True)
             if status.returncode == 0:
                 self.logger.info('Restarted {}\n'.format(self.program_name))
             else:
-                raise Exception('Restart error')
+                raise Exception('Restart error: {}'.format(status.stderr))
         except Exception as e:
-            self.logger.error('Restart error: {}\n'.format(e))
+            self.logger.error(e)
 
 
 @click.command()
-@click.option('--url', help='Request url', required=True)
 @click.option('--program-name', '-p', help='Program name (supervisor)', required=True)
+@click.option('--url', help='Request url', required=False)
+@click.option('--max-memory-size', '-m', help='Set max memory size of process (Mb)', required=False)
 @click.option('--request-timeout', '-t', default=60, help='Request timeout (s)')
-def main(url, program_name, request_timeout):
+def main(program_name, url, max_memory_size, request_timeout):
     monitor = Monitor(
         url=url,
+        max_memory_size=max_memory_size,
         program_name=program_name,
         request_timeout=request_timeout
     )
 
-    threading.Thread(target=monitor.run_checker).start()
-    threading.Thread(target=monitor.run_monitor).start()
+    if max_memory_size and url:
+        threading.Thread(target=monitor.run_monitor).start()
+        threading.Thread(target=monitor.run_checker).start()
+    elif max_memory_size:
+        monitor.run_monitor()
+    else:
+        monitor.run_checker()
 
 
 if __name__ == '__main__':
